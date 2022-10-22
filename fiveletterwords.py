6b2f04631abfb193ca941e2a17575f55b0449d7d
neppord <neppord@gmail.com>
2022-10-22 23:07:54 +0200

    Cleanup

--- fiveletterwords.py
+++ fiveletterwords.py
@@ -1,7 +1,3 @@
-import array
-
-from multiprocessing import Pool
-
 bitfield_lookup = {c: 1 << i for i, c in enumerate('aesiorunltycdhmpgkbwfvzjxq')}
 
 
@@ -22,42 +18,38 @@ def load_words():
                 ret[bitfield] = word
     return ret
 
+
 word_sets = load_words()
 all_letters = (1 << 26) - 1
-masks = [(1 << i) - 1 for i in range(1, 27)]
-word_array = array.array('I', sorted(word_sets.keys(), reverse=True))
-array_indexes = [0]
-old_bit_length = 26
-for i, word in enumerate(word_array):
-    if word.bit_length() != old_bit_length:
-        array_indexes.append(i)
-        old_bit_length = word.bit_length()
 by_char = [
     [
         word
         for word in word_sets
         if word & m == word and word & (m + 1 >> 1)
     ]
-    for m in masks
+    for m in ((1 << i) - 1 for i in range(1, 27))
 ]
-print(len(by_char[25]))
-print(len(word_array[array_indexes[0]: array_indexes[1]]))
+
 
 def step(to_compute):
     for lefts, words in to_compute:
         for word in by_char[lefts.bit_length() - 1]:
             if word & lefts == word:
                 yield lefts & ~word, words + (word,)
-def main ():
+
+
+def main():
     queue = step((all_letters ^ 1 << i, ()) for i in range(26))
     queue = step(queue)
     queue = step(queue)
     queue = step(queue)
     queue = step(queue)
     count = 0
-    for _ in queue:
+    for _, words in queue:
+        #print(*(word_sets[w] for w in words))
         count += 1
     print(count)
+
 
 if __name__ == '__main__':
     main()
